de85e724632d78c0cca6410ceebaf62ba1f2a4b3
cami <cbernardez@mail.utdt.edu>
2023-07-05 03:36:51 -0300

    first

---
+++ scripts/index.js
@@ -0,0 +1,260 @@
+let main = d3.select("main");
+let scrolly = main.select("#scrolly");
+let $figure = scrolly.select("figure");
+let wChart = 1000;
+let hChart = wChart * 0.6;
+let dataChart = [];
+let $step;
+
+// initialize the scrollama
+let scroller = scrollama();
+
+// fetch data
+d3.csv("combined.csv", d3.autoType).then(function (data) {
+  dataChart = data;
+  // kick things off
+  init();
+});
+
+function handleStepExit(response) {
+  console.count("classed");
+  d3.select(response.element).classed("is-active", false);
+}
+
+// scrollama event handlers
+function handleStepEnter(response) {
+  $step = d3.select(response.element);
+  $step.classed("is-active", true);
+  console.count("classed")
+  
+  const col = $step.attr("data-color");
+
+  d3.select("body")
+  .style("transition", "background-color 1s ease-in-out") // Add transition property
+  .style("background-color", col); // Change body background color
+
+  // d3.select("#scrolly")
+  //   .style("transition", "background-color 1s ease-in-out") // Add transition property
+  //   .style("background-color", `${col}`); // Change scrolly element background color
+  
+  const key = $step.attr("data-step");
+  createChart(key);
+  const isFinalArticle = $step.node().classList.contains("final");
+
+  if (isFinalArticle) {
+    d3.select("#scrolly figure").classed("is-fixed", true); // Add class to fix the figure
+  } else {
+    d3.select("#scrolly figure").classed("is-fixed", false); // Remove class to allow figure to move
+  }
+
+  // $step.style.backgroundColor ="red";
+}
+
+function handleStepProgress(response) {
+  $step.select(".progress").text(d3.format(".1%")(response.progress));
+}
+
+function init() {
+  scroller
+    .setup({
+      step: "#scrolly article .step",
+      offset: 0.7,
+      //debug: true,
+      //progress: true,
+    })
+    .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit)
+    .onStepProgress(handleStepProgress);
+    window.addEventListener("resize", scroller.resize);
+}
+
+
+
+/* DataViz */
+// function createChart(key) {
+//   let chart = Plot.plot({
+//     width: wChart,
+//     height: hChart,
+//     grid: {
+//       color:"white",
+//     },
+//     marginTop: 50,
+//     marginBottom: 0,
+//     marginLeft: 50,
+//     marginRight: 50,
+//     style:{
+//       background: "transparent",
+//       color: "white",
+//     },
+//     x: {
+//       ticks: 10,
+//       label: key,
+//       axis: "top",
+//       stroke: "white",
+//       fontWeight: "medium",
+//     },
+//     marks: [
+//       Plot.dot(
+//         dataChart,
+//         Plot.dodgeY({
+//           x: key,
+//           padding: 10,
+//           r: 15,
+//           anchor: "middle",
+//           fill: "white",
+//         })
+//       ),
+//       Plot.image(
+//         dataChart,
+//         Plot.dodgeY({
+//           x: key,
+//           padding: 10,
+//           r: 15,
+//           anchor: "middle",
+//           src: "carita",
+//           width: 30,
+//           title: (d) => `${d.nombre}\n${d.edad} años`,
+//         })
+//       ),
+//       // Plot.text(
+//       //   dataChart,
+//       //   Plot.dodgeY({
+//       //     x: key,
+//       //     padding: 10,
+//       //     r: 15,
+//       //     dy: 20,
+//       //     anchor: "middle",
+//       //     text: "apellido",
+//       //     width: 30,
+//       //     fill: "black",
+//       //   })
+//       // ),
+//     ],
+//   });
+
+
+
+  
+
+//   d3.select("#scrolly figure svg").remove();
+//   d3.select("#scrolly figure").append(() => chart);
+// }
+
+
+
+  
+
+  function createChart (key)  {
+    const morningData = dataChart.filter(d => d.horario === 'morning');
+    const noonData = dataChart.filter(d => d.horario === 'noon');
+    const nightData = dataChart.filter(d => d.horario === 'night');
+
+  let chartData;
+    // Assign the corresponding data based on the key
+    if (key === "morning") {
+      chartData = morningData;
+    } else if (key === "noon") {
+      chartData = noonData;
+    } else if (key === "night") {
+      chartData = nightData;
+    }
+    console.log(key);
+    
+    const loudnessValues = chartData.map(d => d.loudness);
+    const meanLoudness = d3.mean(loudnessValues);
+
+    console.log(meanLoudness);
+
+    let chart = Plot.plot({
+      width: wChart,
+      height: hChart,
+      // grid: {
+      //   color:"white",
+      // },
+      marginTop: 50,
+      marginBottom: 50,
+      marginLeft: 50,
+      marginRight: 50,
+      style:{
+        background: "transparent",
+        color: "white",
+      },
+      x: {
+        domain: [0, 3.0],
+      },
+      y: {
+        domain: [-15, 0],
+      },
+      marks: [
+      Plot.dot(chartData,
+        {
+          x: d=>d.msPlayed /d.duration_ms,
+          y: "loudness",
+          anchor: "middle",
+          fill: "white",
+          r:20,
+        }
+      ),
+      Plot.tip(chartData,Plot.pointer(
+        {
+          x: d=>d.msPlayed /d.duration_ms,
+          y: "loudness",
+          //anchor: "middle",
+          fill: "black",
+          r:5,
+          title: (d) => [d.name, d.artist].join("\n\n")
+        })
+      ),
+
+      Plot.gridX({interval: 1, stroke: "white", strokeOpacity: 0.3}),
+      Plot.gridY({interval: 2, stroke: "white", strokeOpacity: 0.3}),
+
+      Plot.axisX({
+        label: "Cantidad de reproducciones",
+        //anchor: "top",
+        //labelAnchor : left,
+        labelArrow : false,
+        fontSize: 15,
+        //color :,
+        ticks: [1,2,3],
+        tickFormat: d3.format(".0f"),
+        tickPadding: 10,
+        tickSize: 0,
+        lineWidth: 8, 
+        marginBottom: 40}),
+
+        Plot.axisY({
+          label: "dB (negativos)",
+          //anchor: "top",
+          //labelAnchor : left,
+          labelArrow : false,
+          fontSize: 15,
+          //color :,
+          ticks: [-14, -12, -10, -8, -6, -4, -2, 0],
+          tickFormat: (d) => Math.abs(d),
+          tickPadding: 20,
+          tickSize: 0,
+          lineWidth: 8, 
+          marginBottom: 40}),
+
+          
+
+          Plot.ruleY([meanLoudness], {stroke: "red", strokeOpacity:0.5, strokeWidth:1}),
+          //Plot.ruleX([1], {stroke: "red",strokeOpacity:0.3}),
+    ],
+    });
+
+    d3.select("#scrolly figure svg").remove();
+    d3.select("#scrolly figure").append(() => chart);
+
+    d3.select(containerId).select("svg").remove();
+ d3.select(containerId).append(() => chart);
+  };
+
+
+  $(document).ready(function() {
+    $("#datepickerButton").click(function() {
+      $("#datepickerContainer").fadeIn();
+      $("#datepicker").datepicker();
+    });
+  });
